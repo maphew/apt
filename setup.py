ca7bf76fa7c44a26d56fb274bc8bc3a3a29a3582
Matt Wilkie <maphew@gmail.com>
2014-10-28 11:46:52 -0700

    trim setup.py back to just apt, add description

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='AptInstaller',
+    name='apt',
+    description='command line package manager for Osgeo4w',
     version='0.3',
-    py_modules=['apt','dapt'],
-    install_requires=['Click'],
+    #py_modules=['apt','dapt'],
+    #install_requires=['Click'],
     entry_points='''
         [console_scripts]
         apt = apt:main
-        dapt = dapt:main
+        #dapt = dapt:main
         '''
     )

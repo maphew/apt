97625fe90bb70ad9d99d2adcec5019a464b3cd19
Matt Wilkie <maphew@gmail.com>
2015-01-08 06:43:03 -0800

    #1 try :__main__ (nope!)
    Started updating following the Python Packaging Guide, and thought maybe
    `apt:__main__` would work. Nope, it doesn't. Also added
    `zip_safe=False` as that was necessary in Arcplus.

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages # Always prefer setuptools over distutils
+from codecs import open # To use a consistent encoding
+from os import path
+
+# Get the long description from the relevant file
+with open('Readme.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='apt',
     description='command line package manager for Osgeo4w',
     version='0.3',
+    url='https://github.com/maphew/arcplus/blob/master/setup.py',
+    license='GPL',
+    classifiers=[
+        'Development Status :: 3 - Alpha',    
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Public License',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='osgeo4w package-manager installer',
     #py_modules=['apt','dapt'],
     #install_requires=['Click'],
     entry_points='''
         [console_scripts]
-        apt = apt:main
+        apt = apt:__main__
         #dapt = dapt:main
-        '''
+        ''',
+    zip_safe=False,
     )

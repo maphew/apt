8d487e20e9f647144b5e58e86700f4b7732dfcf2
Matt.Wilkie <Matt.Wilkie@gov.yk.ca>
2015-03-06 16:49:12 -0800

    a few more steps in building exe release package

--- build-release-pkg.bat
+++ build-release-pkg.bat
@@ -7,6 +7,9 @@ set pkgdir=\o4w-packages\apt\apt-%ver%
 if not exist "%pkgdir%" mkdir "%pkgdir%"
 pushd "%pkgdir%"
 
+xcopy /s/e ..\apt-skeleton .
+    :: contains pre/posinstall actions, update as needed.
+    
 mkdir bin
 mkdir apps\apt
 
@@ -19,3 +22,16 @@ popd
 echo @^"%%~dp0\..\apps\apt\apt.exe^" %%* > "%pkgdir%"\bin\apt.bat
 
 explorer "%pkgdir%"
+
+:: create distribution archive
+tar cvjf ../apt-0.3-1.tar.bz2
+
+:: upload to /osgeo/download/osgeo4w/x86/release/apt
+:: run http://upload.osgeo.org/cgi-bin/osgeo4w-regen.sh
+
+:: test install/upgrade using setup-test.bat and/or apt iteself (might break 'cause of in use files).
+::      wget http://download.osgeo.org/osgeo4w/x86/setup_test.ini
+::      apt -i setup_test.ini new
+::      apt -i setup_test.ini upgrade
+
+

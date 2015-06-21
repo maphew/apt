abfc5f4ed27d1e07a4161891e531c5f245b14f85
Matt Wilkie <maphew@gmail.com>
2015-06-20 22:27:02 -0700

    test if mocking *only* `knownpaths` works (#48)
    After previous commit RTD build worked, and module docs pages were built as well.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['ctypes', 'wintypes', 'knownpaths']
+MOCK_MODULES = ['knownpaths']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,

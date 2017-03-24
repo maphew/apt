90fc931c2d3c49881431f54c65f45a4e936f2d83
Matt Wilkie <maphew@gmail.com>
2017-03-24 16:33:16 -0700

    Build distribution apt.exe archive that excludes unneed dll's
    #53
    
    --HG--
    branch : bit-ness

--- build-exe.bat
+++ build-exe.bat
@@ -9,6 +9,12 @@ pyinstaller ^
   --workpath=%TEMP%\dev\build ^
   --noconfirm ^
   apt-onefile-excludes.spec
+
+pushd %TEMP%\dev\dist
+7z.exe a -sfx7z.sfx apt-exe-full.exe apt\*
+@rem exclude all MS dlls except 'msvcr90.dll'
+7z.exe a -sfx7z.sfx apt-exe-skinny.exe apt\* -r -x!mf*.dll -x!msvcp90.dll -x!msvcm90.dll
+popd
   
 start %TEMP%\dev
 
@@ -20,3 +26,5 @@ Requirements:
 
 osgeo4w: python-core
 pip: requests pyinstaller
+
+7zip
